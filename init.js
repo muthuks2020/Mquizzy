3ee991dc7dee1276d60084eac310bb33ff1ac058
Muthu <muthukumaraswamy.b@searce.com>
2023-04-05 10:12:28 +0530

    add
    commit

---
+++ init.js
@@ -0,0 +1,30 @@
+﻿var init = { 
+     'questions': [ 
+       {
+           'question': 'Searce is a...',
+           'answers': ['Consulting Company','Cloud first company','AI first company','None of the above'],
+			  'correctAnswer': 1
+       },
+       {
+           'question': 'X comes after?',
+           'answers': ['P','W','Y','v'],
+			  'correctAnswer': 2
+       },
+		 {
+           'question': 'Meh stands for..',
+           'answers': ['Eh','/Sigh','What?','Whatever'],
+			  'correctAnswer': 4
+       }
+     ],
+	  'resultComments' :  
+	  {
+		    perfect: 'Albus, is that you?',
+			 excellent: 'Outstanding, noble sir!',
+			 good: 'Exceeds expectations!',
+			 average: 'Acceptable. For a muggle.',
+			 bad: 'Well, that was poor.',
+			 poor: 'Dreadful!',
+			 worst: 'For shame, troll!'
+	  }
+
+ };
